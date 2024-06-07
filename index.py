15cf0ada0dc929c7f67c5b91f6423b0546f0ed8c
plugkevo <munenekevin366@gmail.com>
2024-06-07 21:03:28 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,23 @@
+print("hello world")
+name = "kevin"
+print(name)
+
+num = 'youtube '
+ans=num[2:5]
+print(ans)
+
+num = 'youtube '
+ans=num[2:3]
+print(ans)
+
+num = 'youtube '
+ans=num[:5]
+print(ans)
+
+num = 'youtube '
+ans=num[0:]
+print(ans)
+
+num = 'youtube '
+ans=num[3:]
+print("my" + ans)
