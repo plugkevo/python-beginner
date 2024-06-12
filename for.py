e6c06c7f64311900ef7b3be80749612bba404036
plugkevo <munenekevin366@gmail.com>
2024-06-12 12:32:18 +0300

    for loops

---
+++ for.py
@@ -0,0 +1,21 @@
+for x in range(1,11,):
+    if x==1:
+        print("monday")
+  
+    elif x==2:
+     print("tuesday")
+  
+    elif x==3:
+        print("wednesday")
+  
+    elif x==4:
+        print("thur")
+
+    elif x==5:
+        print("fri")
+    elif x==6:
+        print("sat")
+  
+    elif x==7:
+        print("sun")
+  
