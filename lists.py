dccd76cfb66a3f620489110e552b478051f2dde7
plugkevo <munenekevin366@gmail.com>
2024-06-12 20:45:40 +0300

    lists tuple and sets

---
+++ lists.py
@@ -0,0 +1,24 @@
+nums = [21, 32, 39,4,45]
+print(nums)
+print(nums[0]) # prints 21
+
+names = ["Kevin" " Joy" " ruth"]
+
+mil = [names, nums]
+print(mil)
+
+nums.append(23)
+print(nums)
+
+nums.insert(39,77)
+print(nums)
+
+del nums[2]
+print(nums)
+
+min=min(nums)
+print(min)
+
+max = max(nums)
+print(max)
+
---
+++ tuple.py
@@ -0,0 +1,13 @@
+#tuple uses ()
+#it is immutable - values can't change
+tup = ( 21, 3 , 15, 4)
+print(tup)
+
+#SET
+ #indexing is not supported
+s = {22,56,43,23,4}
+
+s= {43, 56, 65,34,67}
+
+print(s)
+
