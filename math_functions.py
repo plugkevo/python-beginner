650f1f01c5237eef0a1276c64c618aa3627273be
plugkevo <munenekevin366@gmail.com>
2024-07-01 13:20:45 +0300

    operators & math functions

---
+++ math_functions.py
@@ -0,0 +1,28 @@
+import math
+x=math.sqrt(25)
+print(x)
+
+#ceil and floor
+#ceil gives the highest value
+#floor gves the lowest value
+
+y = math.floor(3.9)
+print(y)
+
+y = math.ceil(3.9)
+print(y)
+
+y = math.pow(3,4) #power
+print(y)
+
+print(math.pi)
+
+import math as m
+y = m.sqrt(25)
+print(y)
+
+
+#to not import everything
+from math import sqrt, pow
+y= sqrt(81)
+print(y)
---
+++ operators.py
@@ -0,0 +1,40 @@
+#arithmetic
+#+ - / *
+
+#assignment =
+x = 2
+
+a,b = 2,4
+
+print(a)
+print(b)
+
+#unary operator
+n = 7
+n=-n
+print(n)
+
+#relational operators
+ans= a>b
+print(ans)
+
+ans=a == b
+print(ans)
+
+#not equal to 
+ans= a!=b
+print(ans)
+
+#logical operators
+
+a= 5
+b =4
+ans = a<8 and b<5
+print(ans) #for and both conditions must be true in order to get true
+
+ans = a<8 or b<2
+print(ans) #for or if one is true then you get true
+
+x= True
+ans= not x
+print(ans)
